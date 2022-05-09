3328c7cb27f860e86d40646fd3bf8edd82b0c172
Felix Khafizov <f_khafizov@protonmail.com>
2022-05-09 15:57:25 +0300

    Fix response body

--- netlify/functions/nasa-apod-api.js
+++ netlify/functions/nasa-apod-api.js
@@ -15,7 +15,7 @@ exports.handler = async function(event, context) {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({data}),
+            body: JSON.stringify(data),
         }
     } catch (err) {
         return {
