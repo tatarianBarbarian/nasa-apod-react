652daeb60179b186d479d4efa7ed584b57db6997
Felix Khafizov <f_khafizov@protonmail.com>
2022-05-10 14:42:29 +0300

    Edit condition for mocking server responses

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,14 @@ import {
 } from 'react-query';
 
 async function prepare() {
-  if (import.meta.env.DEV) {
+  if (import.meta.env.MODE === 'development') {
     const { worker } = await import('./mock/serverMock');
     return worker.start()
   }
   return Promise.resolve()
 }
 
-const queyClient = new QueryClient(); // FIXME: Move to App.js with provider
+const queyClient = new QueryClient();
 
 prepare().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
