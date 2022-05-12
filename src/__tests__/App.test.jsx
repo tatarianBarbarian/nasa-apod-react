04e57b54b77f55f93ab2e2a0ba9a5dec58192402
Феликс Хафизов <f.hafizov@reg.ru>
2022-05-12 13:58:23 +0300

    Add integrational tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,9 +1,12 @@
 import { describe } from 'vitest';
 import App from '../App';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
 import { setupServer } from 'msw/node';
 import { handler } from '../mock/handler';
+import userEvent from '@testing-library/user-event'
+import { rest } from 'msw';
+import { API_URL } from '../api';
 
 const server = setupServer(handler);
 
@@ -11,19 +14,34 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-const queryClient = new QueryClient();
+setLogger({
+    error: () => {},
+    warn: console.warn,
+    log: console.log,
+})
 
-const Wrapped = () => (
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
-);
+const Wrapped = () => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false,
+            },
+        },
+    });
+    return (
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    )
+};
 
 describe('App', () => {
     it('should render spinner while fetching data', async () => {
         render(<Wrapped />);
 
-        expect(screen.getByTestId('spinner')).toBeInTheDocument();
+        const spinner = screen.getByTestId('spinner')
+        expect(spinner).toBeInTheDocument();
+        expect(spinner).toBeVisible();
     });
 
     it('should disable controls while fetching data', async () => {
@@ -45,4 +63,32 @@ describe('App', () => {
         expect(screen.getByTestId('todayBtn')).not.toBeDisabled();
         expect(screen.getByTestId('datepicker')).not.toBeDisabled();
     });
+
+    it('should display pic info when asked', async () => {
+        render(<Wrapped />)
+
+        await waitForElementToBeRemoved(() => screen.getByTestId('spinner'));
+        await userEvent.click(screen.getByTestId('aboutBtn'))
+
+        expect(screen.getByText(/today's pic/i)).toBeInTheDocument()
+        expect(screen.getByText(/today's pic/i)).toBeVisible()
+    })
+
+    it('should display error messages from api', async () => {
+        const errorMessage = 'Error happened!';
+
+        server.use(rest.get(API_URL, async (req, res, ctx) => {
+            return res(ctx.json({msg: errorMessage, code: 400}))
+        }))
+
+        render(<Wrapped />)
+
+        await waitForElementToBeRemoved(() => screen.getByTestId('spinner'))
+
+        const message = screen.getByText(new RegExp(errorMessage, 'i'))
+
+        expect(message).toBeInTheDocument()
+        expect(message).toBeVisible()
+        expect(message.textContent).toMatchInlineSnapshot('"Error 400: Error happened!"')
+    })
 })
