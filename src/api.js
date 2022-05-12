0a26d28d008592570750efc7d1ef3113997a296e
Феликс Хафизов <f.hafizov@reg.ru>
2022-05-12 13:56:53 +0300

    Update api module logic

--- src/api.js
+++ src/api.js
@@ -3,13 +3,22 @@ import axios from 'axios';
 const API_URL = '/.netlify/functions/nasa-apod-api';
 
 const fetchApodData = async ({queryKey}) => {
-    const {data} = await axios.get(API_URL, {
-        params: {
-            date: queryKey[1]
+    try {
+        const {data} = await axios.get(API_URL, {
+            params: {
+                date: queryKey[1]
+            }
+        });
+        
+        if (data.code) {
+            throw new Error(`Error ${data.code}: ${data.msg}`);
         }
-    });
 
-    return data;
+        return data;
+    }
+    catch(err) {
+        throw err;
+    }
   };
 
 export {fetchApodData, API_URL};
