1a5d770cb55076137b5cfe62d78c510ea4cc87ec
Феликс Хафизов <f.hafizov@reg.ru>
2022-05-13 10:43:59 +0300

    Add tests todos for date reducer

---
+++ src/__tests__/useDateReducer.test.js
@@ -0,0 +1,8 @@
+import { describe } from "vitest";
+
+describe('useDateReducer', () => {
+    it.todo('correctly increments date');
+    it.todo('correctly decrements date');
+    it.todo('correctly sets custom date');
+    it.todo('correctly resets date');
+})
