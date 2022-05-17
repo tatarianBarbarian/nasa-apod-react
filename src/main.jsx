803ec8752e8c15c732a0d4002805e8cf4d7474cf
Феликс Хафизов <f.hafizov@reg.ru>
2022-05-17 11:37:43 +0300

    Remove console.log

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import {
 } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-console.log(import.meta.env);
-
 async function prepare() {
   if (import.meta.env.DEV && !import.meta.env.TEST) {
     const { worker } = await import('./mock/worker');
